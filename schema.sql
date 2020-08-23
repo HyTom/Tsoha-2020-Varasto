ad2a1442d9dd0f3ff7fe6205f8f07287ba06c1d4
unknown <hyyrylainenTommi@gmail.com>
2020-08-23 21:08:17 +0300

    lisayksia

--- schema.sql
+++ schema.sql
@@ -1,2 +1,2 @@
-CREATE TABLE tavaratest (id SERIAL PRIMARY KEY, TEXT content);
-CREATE TABLE tavaratest (id SERIAL PRIMARY KEY, TEXT username, TEXT password, TEXT position);
+CREATE TABLE tavaratest (id SERIAL PRIMARY KEY, content TEXT);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, position TEXT);
